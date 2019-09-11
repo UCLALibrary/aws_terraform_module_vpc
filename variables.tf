f25fabe9ab23db8cd4c2586550d4a73663ac7b73
Anthony Vuong <vuonga91@gmail.com>
2019-09-11 02:33:49 -0700

    debugging

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "private_subnet_count" { default = 0 }
 
 variable "enable_nat" { default = 0 }
 variable "associate_existing_nat" { default = 0 }
-variable "existing_private_nat_gateway_id" { default = "" }
+variable "existing_private_nat_gateway_id" { default = null }
 
 variable "vpc_endpoint" {}
 variable "create_vpc_endpoint" { default = 0 }
