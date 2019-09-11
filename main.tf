26f8036e2445641ee057437fcd1b5dac37c8a044
Anthony Vuong <vuonga91@gmail.com>
2019-09-10 22:51:58 -0700

    Add private subnet capabilities

--- main.tf
+++ main.tf
@@ -13,11 +13,19 @@ resource "aws_vpc" "main" {
 # Create public subnet to attach Internet Gateway route
 #############################################################################################################
 resource "aws_subnet" "public" {
-  count                   = "${var.subnet_count}"
+  count                   = "${var.public_subnet_count}"
   vpc_id                  = "${aws_vpc.main.id}"
-  cidr_block              = "${cidrsubnet(aws_vpc.main.cidr_block, 8, var.subnet_init_value + count.index)}"
+  cidr_block              = "${cidrsubnet(aws_vpc.main.cidr_block, 8, var.public_subnet_init_value + count.index)}"
   availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
   map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  count                   = "${var.private_subnet_count}"
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr_block              = "${cidrsubnet(aws_vpc.main.cidr_block, 8, var.private_subnet_init_value + count.index)}"
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+  map_public_ip_on_launch = false
 }
 
 #############################################################################################################
@@ -27,12 +35,17 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = "${aws_vpc.main.id}"
 }
 
-#############################################################################################################
-# Set main route table for VPC to Internet Gateway
-#############################################################################################################
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.main.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.gw.id}"
+resource "aws_route_table" "route_table_ig_gw" {
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+}
+
+resource "aws_route_table_association" "route_public_subnets" {
+  count = "${var.public_subnet_count}"
+  subnet_id = "${aws_subnet.public[count.index - 1]}"
+  route_table_id = "${aws_route_table.route_table_ig_gw.id}"
 }
 
--- variables.tf
+++ variables.tf
@@ -2,6 +2,9 @@ variable "vpc_cidr_block" {
   default = "10.0.0.0/24"
 }
 
-variable "subnet_init_value" {}
-variable "subnet_count" { default = 1 }
+variable "public_subnet_init_value" {}
+variable "public_subnet_count" { default = 1 }
+
+variable "private_subnet_init_value" {}
+variable "private_subnet_count" { default = 0 }
 
