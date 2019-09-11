fbadd6477e569a14dec6579122708c2947b812da
Anthony Vuong <vuonga91@gmail.com>
2019-09-11 01:47:59 -0700

    test fix

--- main.tf
+++ main.tf
@@ -50,19 +50,19 @@ resource "aws_route_table_association" "route_public_subnets" {
 }
 
 resource "aws_eip" "private_nat_eip" {
-  count = "${var.private_subnet_count > 0 ? 1 : 0}"
+  count = "${var.enable_nat > 0 ? 1 : 0}"
   vpc   = true
 }
 
 resource "aws_nat_gateway" "private_nat_gw" {
-  count = "${var.private_subnet_count > 0 ? 1 : 0}"
+  count = "${var.enable_nat > 0 ? 1 : 0}"
   allocation_id = "${aws_eip.private_nat_eip[0].id}"
   subnet_id = "${aws_subnet.public[0].id}"
   depends_on = ["aws_internet_gateway.gw"]
 }
 
 resource "aws_route_table" "route_table_private_nat" {
-  count = "${var.private_subnet_count > 0 ? 1 : 0}"
+  count = "${var.enable_nat > 0 ? 1 : 0}"
   vpc_id = "${aws_vpc.main.id}"
   route {
     cidr_block = "0.0.0.0/0"
@@ -71,7 +71,7 @@ resource "aws_route_table" "route_table_private_nat" {
 }
 
 resource "aws_route_table_association" "route_private_subnets" {
-  for_each = toset(aws_subnet.private.*.id)
+  for_each  = var.enable_nat > 0 ? toset(aws_subnet.private.*.id) : []
   subnet_id = each.key
   route_table_id = "${aws_route_table.route_table_private_nat[0].id}"
 }
