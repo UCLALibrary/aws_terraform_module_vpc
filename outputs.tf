a52be2e8a077b6c9bfe1cf29329ff93572b17121
Anthony Vuong <avuong@cachemeoutside.io>
2019-12-20 09:07:51 -0800

    [IIIF-603] Make VPC module output IDs for route tables and gateways (#4)

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,15 @@ output "vpc_private_subnet_ids" {
   value = "${aws_subnet.private.*.id}"
 }
 
+output "public_network_route_table_id" {
+  value = "${aws_route_table.route_table_ig_gw.id}"
+}
+
+output "private_network_route_table_id" {
+  value = "${aws_route_table.route_table_private_nat[0].id}"
+}
+
+output "private_nat_gateway_id" {
+  value = "${aws_nat_gateway.private_nat_gw[0].id}"
+}
+
